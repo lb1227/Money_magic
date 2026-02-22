81df80f95bafdc6881f90cc10ef9554155d6a401
Luke <117487478+lb1227@users.noreply.github.com>
2026-02-22 03:31:26 -0500

    Update supabase.js

--- client/src/lib/supabase.js
+++ client/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://edliniyruqjvrmyxsxgk.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbGluaXlydXFqdnJteXhzeGdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzA2NzgsImV4cCI6MjA4NzMwNjY3OH0.PCx7HVzAPn-vDlBRBVPMrKZ1T2BH5nj_i-cGQVj4Cus'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // eslint-disable-next-line no-console
